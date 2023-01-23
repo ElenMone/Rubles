394f3eb2e0d446a0cdf4f7d526a976c8cfe24d29
Elena <laaarsen@mail.ru>
2023-01-24 03:13:37 +0400

    Name

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 300;
-        int a = 1200;
-        if (a > 1000) {
+        int balance = 300;
+        int entrance = 1200;
+        if ( entrance > 1000) {
             System.out.println("Начислено бонусных рублей:");
         }  else {
             System.out.println("Бонусы не начислены");
         }
-        int b = (a / 100);
-            System.out.println(b);
-        int i = b + x + a;
-            System.out.println("Баланс " + i);
+        int bonus = (entrance / 100);
+            System.out.println(bonus);
+        int result = bonus + entrance + balance;
+            System.out.println("Баланс " + result);
     }
 }
